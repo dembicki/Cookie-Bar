cadb485f4004d187861eed20bbd4ab867b1565f6
Damian <damian@MacBook-Pro.local>
2020-02-28 10:32:28 +0100

    Create button for showing widget

--- widget.js
+++ widget.js
@@ -6,28 +6,41 @@ class Bar {
         }
 
         createBar(obj.message, obj.position);
+        createShowBarButton();
     }
 
 }
 
-function createBar(message, position){
+ function createBar(message, position){
     let x = document.createElement('div');
     x.classList.add('wrapper');
     x.innerHTML = message;
     document.body.appendChild(x);
+    x.style.display = 'none';
 
     if(position == 'top'){
         x.style.top = '0';
     }else if (position== 'bottom'){
         x.style.bottom = '0';
     }
-    else{
-        x.style.display = 'none';
-    }
 }
 
+function createShowBarButton(){
+    let showBar = document.createElement('a');
+    showBar.classList.add('showBar');
+    showBar.innerHTML = 'Show Bar';
+    document.body.appendChild(showBar);
+    let bar = document.querySelector('.wrapper');
+
+    showBar.addEventListener('click', () => {
+        bar.style.display = 'block';
+    })
+}; 
+
+
+
 let myBar = new Bar ({
-    message:'lorem ipsum',
+    message:'Add beautiful widgets to your website',
     position:'top'
 }
 );
